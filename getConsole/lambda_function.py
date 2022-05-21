db0e7e05b71e0104a9a13fe772cc584936442320
JamesWithCode <minseok.jeong.james@gmail.com>
2022-05-22 02:44:03 +0900

    getConsole api 개발 완료

--- getConsole/lambda_function.py
+++ getConsole/lambda_function.py
@@ -17,14 +17,14 @@ def getMysqlConn():
             password=DB_PASSWORD, 
             db=DB_NAME)
 
-def  conductSqlQuery(sql):
+def  conductSqlQuery(sql,sqlData):
     print("--------------------------------")
     print("conductSqlQuery function init")
     conn=getMysqlConn()
     curs = conn.cursor()
     with conn:
         with conn.cursor(pymysql.cursors.DictCursor) as cur:
-                cur.execute(sql)
+                cur.execute(sql,sqlData)
                 result = json.dumps(cur.fetchall(),default=json_default,ensure_ascii = False)
     print("conductSqlQuery function done")
     print("--------------------------------")
@@ -38,17 +38,20 @@ def json_default(value):
 def lambda_handler(event, context):
     try:
         print("--------------------------------")
-        print("getAllConsoles lambda_handler function init")
-
+        print("getConsole lambda_handler function init")
+        params=event['queryStringParameters']
+        consoleId=params['consoleId']
+        print("consoleId : ",consoleId)
+        
         conn=getMysqlConn()
-        sql = "SELECT * FROM console"
-        retValue=conductSqlQuery(sql)
+        sql = "SELECT * FROM console WHERE consoleId= %s"
+        retValue=conductSqlQuery(sql,consoleId)
         
         result={
             'statusCode':HTTPStatus.OK,
             'body':retValue
         }
-        print("getAllConsoles lambda_handler function done")
+        print("getConsole lambda_handler function done")
         print("--------------------------------")
         return result
     except Exception:
